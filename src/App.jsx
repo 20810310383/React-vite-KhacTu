84f97a6c8f5dd5df4962afd4e0094355f1266562
dokhactu <dokhactu1822@gmail.com>
2024-06-24 17:33:12 +0700

    update vid28

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// component = html + css + js
+const MyComponent = () => {
+  return (
+    <>
+      <div>Khắc Tú học React</div>
+    </>
+  )
+}
+
 // () => {}
 const App = () => {
   const [count, setCount] = useState(0)
 
-  const myFunction =  () => {
-    console.log("hello");
-  }
-  myFunction()
+  
 
   return (
     <>
@@ -23,6 +29,7 @@ const App = () => {
         </a>
       </div>
       <h1>Hello Khắc Tú</h1>
+      <MyComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
