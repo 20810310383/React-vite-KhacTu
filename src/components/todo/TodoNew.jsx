392002aaa9f3b6c7ea1207e7935edf653a2b211c
dokhactu <dokhactu1822@gmail.com>
2024-06-24 23:45:52 +0700

    update vid36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -3,10 +3,19 @@ const TodoNew = (props) => {
     console.log("check props: ", props);
     const {addNewTodo} = props
 
+    const handleClick = () => {
+        alert("click me")
+    }
+
+    const handleOnChange = (name) => {
+        console.log(">> handleOnChange: ", name);
+    }
+
+
     return (
         <>
-            <input className='inpTxt' placeholder='Enter your task' type="text" />
-            <button className='btnAdd' >Add</button>
+            <input className='inpTxt' placeholder='Enter your task' type="text" onChange={(event) => {handleOnChange(event.target.value)}} />
+            <button className='btnAdd' onClick={handleClick} >Add</button>
         </>
     )
 }
